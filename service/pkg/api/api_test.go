5e302943bdd3448a9c0b3393b04166be87836062
Farye Nwede <farye@aeekay.com>
2023-06-04 15:11:39 +0000

    Adding API test for ping

--- service/pkg/api/api_test.go
+++ service/pkg/api/api_test.go
@@ -4,9 +4,12 @@ package api
 import (
 	"net/http"
 	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
 
 	"github.com/aeekayy/stilla/service/pkg/models"
-	"github.com/gin-gonic/gin"
 )
 
 // GetTestGinContext creates a Gin context for tests
@@ -24,4 +27,16 @@ func GetTestGinContext() *gin.Context {
 
 func getConfig() *models.Config {
 	return models.NewConfig()
+}
+
+func TestPingRoute(t *testing.T) {
+	dal := setupDep(t)
+	router := NewRouter(dal)
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/api/v1/health/", nil)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, "{\"message\":\"pong\"}", w.Body.String())
 }
